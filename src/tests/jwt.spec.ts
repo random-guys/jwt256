3a609220d48ea1cbbcf5018fb656b530e1019b53
Raymond Tukpe <rtukpe@gmail.com>
2019-02-24 21:48:38 +0100

    chore: quotes changed because of TS compiler settings

--- src/tests/jwt.spec.ts
+++ src/tests/jwt.spec.ts
@@ -1,15 +1,15 @@
-import { GetUserAuth, SetUserAuth } from '../lib/jwt';
-import RedisService from '../lib/redis';
-import express from 'express';
-import supertest from 'supertest';
+import { GetUserAuth, SetUserAuth } from "../lib/jwt";
+import RedisService from "../lib/redis";
+import express from "express";
+import supertest from "supertest";
 
 let redisService: RedisService;
-const user_id = '12345-67890-12345-67890-12345-67890';
+const user_id = "1a16acbd-ae44-444c-9271-4bc5504b127c";
 
 beforeAll(async () => {
-  process.env.JWT_SECRET = '123432345432345432343';
-  process.env.REDIS_URL = 'redis://localhost:6379';
-  process.env.ENCRYPTION_KEY = '4844e2650b69fd92f0af204275ca74b9';
+  process.env.JWT_SECRET = "123432345432345432343";
+  process.env.REDIS_URL = "redis://localhost:6379";
+  process.env.ENCRYPTION_KEY = "4844e2650b69fd92f0af204275ca74b9";
 
   redisService = new RedisService(process.env.REDIS_URL);
 });
@@ -18,32 +18,32 @@ afterAll(async () => {
   redisService.quit();
 });
 
-it('should create jwt and store it in Redis', async () => {
+it("should create jwt and store it in Redis", async () => {
   expect.assertions(1);
 
-  console.time('jwt');
+  console.time("jwt");
   const auth_key = await SetUserAuth(redisService, user_id);
-  console.timeEnd('jwt');
+  console.timeEnd("jwt");
 
   const redis_key = await redisService.get(user_id);
   expect(redis_key).toBe(auth_key);
 });
 
-it('should validate auth', async () => {
+it("should validate auth", async () => {
   expect.assertions(2);
   const app = express();
 
-  console.time('jwt');
-  app.get('/', GetUserAuth(redisService), (req, res, next) => {
+  console.time("jwt");
+  app.get("/", GetUserAuth(redisService), (req, res, next) => {
     expect(req.user).toBe(user_id);
-    console.timeEnd('jwt');
-    res.status(200).json({ message: 'Hello World!' });
+    console.timeEnd("jwt");
+    res.status(200).json({ message: "Hello World!" });
   });
 
   const res = await supertest(app)
-    .get('/')
-    .set('Authorization', `Bearer ${await redisService.get(user_id)}`)
+    .get("/")
+    .set("Authorization", `Bearer ${await redisService.get(user_id)}`)
     .expect(200);
 
-  expect(res.body.message).toBe('Hello World!');
+  expect(res.body.message).toBe("Hello World!");
 });
