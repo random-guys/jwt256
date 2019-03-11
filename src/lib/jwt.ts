defa3ceaed624426f5ba902907d4a291ae5229c6
Raymond Tukpe <rtukpe@gmail.com>
2019-03-11 13:48:24 +0100

    feat: token now is refeshed when a request is made

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -6,58 +6,63 @@ import httpStatus from "http-status-codes";
 import RedisService from "./redis";
 import { AES256, JWT } from "./utils";
 
-export const GetUserAuth = (redis: RedisService) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { JWT_SECRET, ENCRYPTION_KEY } = process.env;
+const GetAuth = (redis: RedisService) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { JWT_SECRET, ENCRYPTION_KEY } = process.env;
 
-    if (!JWT_SECRET) {
-      throw new Error("please add JWT_SECRET to the environment variables");
+      if (!JWT_SECRET) {
+        throw new Error("please add JWT_SECRET to the environment variables");
+      }
+
+      if (!ENCRYPTION_KEY) {
+        throw new Error(
+          "please add ENCRYPTION_KEY to the environment variables"
+        );
+      }
+
+      const jwt: JWT = new JWT(JWT_SECRET);
+
+      const auth = req.headers.authorization;
+      if (!auth) throw new Error("Session has expired");
+      const auth_key = auth.split(" ")[1];
+
+      // split the keys using the separator
+      const keys = auth_key.split(".");
+      const cipher = keys[0];
+      const tag = keys[2];
+      const iv = keys[1];
+
+      const aes = new AES256(Buffer.from(ENCRYPTION_KEY, "utf8"));
+      const plain_text = aes.decrypt(
+        cipher,
+        Buffer.from(iv, "hex"),
+        Buffer.from(tag, "hex")
+      );
+
+      const token = await jwt.decode(plain_text);
+      const user = await redis.get(token.id);
+      if (user === auth_key) {
+        // Auth was successful
+        req.user = token.id;
+
+        // refresh access token
+        await SetAuth(redis, token.id);
+
+        next();
+      } else throw new Error("Session has expired");
+    } catch (err) {
+      return res.status(httpStatus.UNAUTHORIZED).json({
+        status: "error",
+        data: null,
+        message: err.message,
+        code: httpStatus.UNAUTHORIZED
+      });
     }
-
-    if (!ENCRYPTION_KEY) {
-      throw new Error("please add ENCRYPTION_KEY to the environment variables");
-    }
-
-    const jwt: JWT = new JWT(JWT_SECRET);
-
-    const auth = req.headers.authorization;
-    if (!auth) throw new Error("Session has expired");
-    const auth_key = auth.split(" ")[1];
-
-    // split the keys using the separator
-    const keys = auth_key.split(".");
-    const cipher = keys[0];
-    const tag = keys[2];
-    const iv = keys[1];
-
-    const aes = new AES256(Buffer.from(ENCRYPTION_KEY, "utf8"));
-    const plain_text = aes.decrypt(
-      cipher,
-      Buffer.from(iv, "hex"),
-      Buffer.from(tag, "hex")
-    );
-
-    const token = await jwt.decode(plain_text);
-    const user = await redis.get(token.id);
-    if (user === auth_key) {
-      req.user = token.id;
-      next();
-    } else throw new Error("Session has expired");
-  } catch (err) {
-    return res.status(httpStatus.UNAUTHORIZED).json({
-      status: "error",
-      data: null,
-      message: err.message,
-      code: httpStatus.UNAUTHORIZED
-    });
-  }
+  };
 };
 
-export const SetUserAuth = async (redis: RedisService, user_id: string) => {
+const SetAuth = async (redis: RedisService, user_id: string) => {
   const { JWT_SECRET, ENCRYPTION_KEY } = process.env;
 
   if (!JWT_SECRET) {
@@ -92,3 +97,6 @@ export const SetUserAuth = async (redis: RedisService, user_id: string) => {
   await redis.set(user_id, auth_key, "EX", JWT_CACHE_TTL);
   return auth_key;
 };
+
+export const SetUserAuth = SetAuth;
+export const GetUserAuth = GetAuth;
