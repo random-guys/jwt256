ca48c5e1c356740524010319c97f4aa7cac4228d
Raymond Tukpe <rtukpe@gmail.com>
2019-03-12 14:08:00 +0100

    chore: updated tests

--- src/tests/jwt.spec.ts
+++ src/tests/jwt.spec.ts
@@ -5,6 +5,7 @@ import supertest from "supertest";
 
 let redisService: RedisService;
 const user_id = "1a16acbd-ae44-444c-9271-4bc5504b127c";
+let redis_key = "";
 
 beforeAll(async () => {
   process.env.JWT_SECRET = "123432345432345432343";
@@ -25,12 +26,12 @@ it("should create jwt and store it in Redis", async () => {
   const auth_key = await SetUserAuth(redisService, user_id);
   console.timeEnd("jwt");
 
-  const redis_key = await redisService.get(user_id);
+  redis_key = await redisService.get(user_id);
   expect(redis_key).toBe(auth_key);
 });
 
 it("should validate auth", async () => {
-  expect.assertions(2);
+  expect.assertions(4);
   const app = express();
 
   console.time("jwt");
@@ -40,10 +41,22 @@ it("should validate auth", async () => {
     res.status(200).json({ message: "Hello World!" });
   });
 
+  app.get("/hi", GetUserAuth(redisService), (req, res, next) => {
+    expect(req.user).toBe(user_id);
+    console.timeEnd("jwt");
+    res.status(200).json({ message: "Hi World!" });
+  });
+
   const res = await supertest(app)
     .get("/")
-    .set("Authorization", `Bearer ${await redisService.get(user_id)}`)
+    .set("Authorization", `Bearer ${redis_key}`)
+    .expect(200);
+
+  const anotherRes = await supertest(app)
+    .get("/hi")
+    .set("Authorization", `Bearer ${redis_key}`)
     .expect(200);
 
   expect(res.body.message).toBe("Hello World!");
+  expect(anotherRes.body.message).toBe("Hi World!");
 });
