0a4f9c03802cf4c901981520f7c41e135cbd8f00
Raymond Tukpe <rtukpe@gmail.com>
2019-04-01 15:32:12 +0100

    feat: added types to encode and decode functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import jwt from "jsonwebtoken";
-import crypto, { CipherGCMTypes } from "crypto";
+import jwt from 'jsonwebtoken';
+import crypto, { CipherGCMTypes } from 'crypto';
 
 export class JWT {
   private jwt_secret: string;
@@ -8,16 +8,16 @@ export class JWT {
     this.jwt_secret = secret;
   }
 
-  sign = (payload: any) => {
+  sign = (payload: any): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
-      jwt.sign(payload, this.jwt_secret, { expiresIn: "24h" }, (err, token) => {
+      jwt.sign(payload, this.jwt_secret, { expiresIn: '24h' }, (err, token) => {
         if (err) reject(err);
         resolve(token);
       });
     });
   };
 
-  decode = <T = any>(token: string) => {
+  decode = <T = any>(token: string): Promise<T> => {
     return new Promise<T>((resolve, reject) => {
       jwt.verify(token, this.jwt_secret, (err, decoded: any) => {
         if (err) reject(err);
@@ -29,7 +29,7 @@ export class JWT {
 
 export class AES256 {
   private secret_key: Buffer;
-  private ALGO: CipherGCMTypes = "aes-256-gcm";
+  private ALGO: CipherGCMTypes = 'aes-256-gcm';
 
   constructor(key) {
     this.secret_key = key;
@@ -37,11 +37,11 @@ export class AES256 {
 
   // encrypt returns base64-encoded ciphertext
   encrypt = (str: string) => {
-    const iv = Buffer.from(crypto.randomBytes(16).toString("utf8"), "utf8");
+    const iv = Buffer.from(crypto.randomBytes(16).toString('utf8'), 'utf8');
     const cipher = crypto.createCipheriv(this.ALGO, this.secret_key, iv);
 
-    let enc = cipher.update(str, "utf8", "base64");
-    enc += cipher.final("base64");
+    let enc = cipher.update(str, 'utf8', 'base64');
+    enc += cipher.final('base64');
     return { enc, iv, tag: cipher.getAuthTag() };
   };
 
@@ -49,8 +49,8 @@ export class AES256 {
   decrypt = (enc: string, iv: Buffer, authTag: Buffer) => {
     const decipher = crypto.createDecipheriv(this.ALGO, this.secret_key, iv);
     decipher.setAuthTag(authTag);
-    let str = decipher.update(enc, "base64", "utf8");
-    str += decipher.final("utf8");
+    let str = decipher.update(enc, 'base64', 'utf8');
+    str += decipher.final('utf8');
     return str;
   };
 }
