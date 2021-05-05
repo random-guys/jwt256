862f5f74953c9ff276473de6f854b4ba46063e9d
Raymond Tukpe <rtukpe@gmail.com>
2021-05-06 00:23:29 +0100

    chore: update redis client

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,46 +1,48 @@
-import { promisify } from "util";
 import redis, { RedisClient } from "redis";
+import { promisify } from "util";
 
-export default class RedisService implements IRedisService {
+export default class RedisService {
   redis: RedisClient;
 
-  get: (key: string) => Promise<string>;
-  hgetall: (key: string) => Promise<object>;
+  del: (key: string) => Promise<number>;
+  expire: (key: string, seconds: number) => Promise<number>;
+  expireat: (key: string, timestamp: number) => Promise<number>;
+  incrby: (key: string, increment: number) => Promise<number>;
   hdel: (hash: string, field: string) => Promise<any>;
   hget: (hash: string, field: string) => Promise<string>;
+  hgetall: (key: string) => Promise<object>;
   hset: (hash: string, field: string, value: any) => Promise<any>;
-  quit: () => Promise<void>;
   set: (
     key: string,
     value: any,
     mode?: string,
     duration?: number
   ) => Promise<any>;
+  get: (key: string) => Promise<string>;
+  quit: () => Promise<void>;
+  flushall: (async: "ASYNC") => Promise<string>;
 
   constructor(url: string) {
     this.redis = redis.createClient({ url });
-    this.hdel = promisify(this.redis.hdel).bind(this.redis);
-    this.hget = promisify(this.redis.hget).bind(this.redis);
-    this.hgetall = promisify(this.redis.hgetall).bind(this.redis);
-    this.hset = promisify(this.redis.hset).bind(this.redis);
-    this.set = promisify(this.redis.set).bind(this.redis);
-    this.get = promisify(this.redis.get).bind(this.redis);
-    this.quit = promisify(this.redis.quit).bind(this.redis);
 
-    this.redis.on("error", err => {
-      console.log("An error occured while initialzing redis client");
-      console.log(err);
+    const commands = [
+      "del",
+      "expire",
+      "expireat",
+      "incrby",
+      "hdel",
+      "hget",
+      "hgetall",
+      "flushall",
+      "hset",
+      "set",
+      "get",
+      "quit",
+    ];
+
+    // Promisify all the specified commands
+    commands.forEach((command) => {
+      this[command] = promisify(this.redis[command]).bind(this.redis);
     });
   }
-}
-
-export interface IRedisService {
-  redis: RedisClient;
-  hdel(hash: string, field: string): Promise<any>;
-  hget(hash: string, field: string): Promise<string>;
-  hgetall(key: string): Promise<object>;
-  hset(hash: string, field: string, value: any): Promise<any>;
-  set(key: string, value: any, mode?: string, duration?: number): Promise<any>;
-  get(key: string): Promise<string>;
-  quit(): Promise<void>;
 }
